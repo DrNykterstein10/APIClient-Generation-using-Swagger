fae8b7ca4d14ab65d191fc50e338e9c6cd6c4809
Piyush Rai <piyush.rai.official@gmail.com>
2021-07-30 10:34:44 +0530

    First commit

---
+++ EMICalculatorAPIClient/EMICalculatorAPIClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EMICalculatorAPIClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new EMICalculatorClient();
+            var emiDetails = await client.GetEMIDetailsAsync(10000, 24, 14);
+
+            
+                    
+        }
+    }
+}
